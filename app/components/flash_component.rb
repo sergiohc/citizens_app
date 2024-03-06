bb9ae1106aa3a07ef45eb077167cc73534f3fb92
sergiohc <sergioh.c@live.com>
2024-03-06 15:08:19 -0300

    adjust flash messages

--- app/components/flash_component.rb
+++ app/components/flash_component.rb
@@ -1,9 +1,22 @@
 # frozen_string_literal: true
 
 class FlashComponent < ViewComponent::Base
+  include ApplicationHelper
+
   def initialize(id:, type:, message:)
     @id = id
     @type = type
     @message = message
+  end
+
+  private
+
+  def color
+    case @type
+    when 'success' then 'green'
+    when 'danger' then 'red'
+    when 'warning' then 'yellow'
+    else 'gray'
+    end
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,10 @@ module ApplicationHelper
       render(FlashComponent.new(id: "toast-#{type}", message: message, type: type))
     end.join.html_safe # rubocop:disable Rails/OutputSafety
   end
+
+  def show_icon(path)
+    File.open("app/assets/images/icons/#{path}", "rb") do |file|
+      raw file.read
+    end
+  end
 end
