d0daf794916decd36b035018c68fabe803565969
sergiohc <sergioh.c@live.com>
2024-03-12 17:03:07 -0300

    fix tailwind component views

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ module.exports = {
     './app/helpers/**/*.rb',
     './app/assets/stylesheets/**/*.css',
     './app/javascript/**/*.js',
+    './app/components/**/*.{erb,html}',
     './node_modules/flowbite/**/*.js'
   ],
 
